1189e3700bae7a9c71ba8aea4e24839723d85753
harish-kumar-kp <129352109+harish-kumar-kp@users.noreply.github.com>
2026-07-21 00:00:37 +0530

    Update data.js

--- VIdOTT/data/data.js
+++ VIdOTT/data/data.js
@@ -163,7 +163,7 @@
       title: "Dunston Checks In 1996",
       src: "https://dn721901.ca.archive.org/0/items/Popcornarchive-dunstonChecksIn1996/Popcornarchive-dunstonChecksIn1996.mp4",
       thumb: "https://m.media-amazon.com/images/S/pv-target-images/318ccb307fe9021f36adf9cd36c520d04d0a64aa594088c66741a770549e2ff2.png",
-	  subtitle: "https://raw.githubusercontent.com/harish-kumar-kp/showreel/main/VIdOTT/Dunston.Checks.In.en.srt"
+	  subtitle: "https://raw.githubusercontent.com/harish-kumar-kp/showreel/main/VIdOTT/DunstonChecksIn1996.srt"
     },
     {
       title: "Lassie 2005",
